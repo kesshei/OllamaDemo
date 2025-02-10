0ca50f5e179f216e2e0eafe46bf37ceb92e417d6
kesshei <dagang520@qq.com>
2025-02-10 22:01:28 +0800

    提交代码

---
+++ OllamaDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using Azure;
+using Codeblaze.SemanticKernel.Connectors.Ollama;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel;
+using Microsoft.VisualBasic;
+using System.ClientModel;
+
+namespace OllamaDemo
+{
+    internal class Program
+    {
+    static async Task Main(string[] args)
+    {
+        var builder = Kernel.CreateBuilder().AddOllamaChatCompletion("deepseek-r1:32b", "http://192.168.0.120:11434");
+
+        builder.Services.AddScoped<HttpClient>();
+        var kernel = builder.Build();
+
+        while (true)
+        {
+            string input = "";
+            Console.Write("请输入: ");
+            input = Console.ReadLine();
+            Console.WriteLine("");
+            var contents = kernel.InvokePromptStreamingAsync(input);
+
+            if (contents == null)
+            {
+                Console.WriteLine("Error: 内容为空!");
+                continue;
+            }
+            else
+            {
+                Console.WriteLine($"\nDeepseek: \n");
+                await foreach (var item in contents)
+                {
+                    Console.Write(item.ToString());
+                }
+            }
+            Console.WriteLine("");
+        }
+    }
+    }
+}
+
